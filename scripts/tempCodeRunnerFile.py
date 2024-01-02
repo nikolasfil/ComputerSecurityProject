8280957f9a9aa769723f279d8e799e5e3970c43d
nikolasfil <nikolas13in@gmail.com>
2024-01-03 01:16:59 +0200

    initial update

---
+++ scripts/tempCodeRunnerFile.py
@@ -0,0 +1,56 @@
+#!/bin/python
+import sys
+from pathlib import Path
+import base64
+
+
+def main():
+    path = Path(__file__).parent
+    direct_parent = path.parent
+    file_path = Path(direct_parent, "files")
+
+    users = {
+        "Smith": " WDdNUWtQM1cyOWZld0hkQw==",
+        "Lucas": " c3VSSkFkR3dMcDhkeTNyRg==",
+        "Travis": " REpjZVZ5OThXMjhZN3dMZw==",
+        "Dexter": " N1p3VjRxdGc0MmNtVVhHWA==",
+        "Meyer": " Y3FObkJXQ0J5UzJEdUpTeQ==",
+    }
+
+    for user in users:
+        user = user.strip()
+        file = Path(file_path, f"{user}.b64")
+        with open(file, "w") as f:
+            f.write(users[user])
+
+    passwords = {user: "" for user in users}
+
+    for file in file_path.iterdir():
+        with open(file, "r") as f:
+            passwords[file.stem] = f.readline().strip("\n")
+
+    # decode base64 encoding
+
+    for user in passwords:
+        # passwords[user] = passwords[user].decode("base64")
+        passwords[user] = base64.b64decode(passwords[user]).decode("utf-8")
+        with open(Path(file_path, f"{user}.txt"), "w") as f:
+            f.write(passwords[user])
+
+    users_file = Path(file_path, "users.txt")
+    with open(users_file, "w") as f:
+        for user in passwords:
+            user = user.strip()
+            f.write(f"{user}\n")
+            f.write(f"{user.lower()}\n")
+
+    passwords_file = Path(file_path, "passwords.txt")
+    with open(passwords_file, "w") as f:
+        for user in passwords:
+            user = user.strip()
+            password = passwords[user].strip()
+            f.write(f"{password}\n")
+
+
+if __name__ == "__main__":
+    main()
